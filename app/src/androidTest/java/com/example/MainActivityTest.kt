45c8aa4e8b1bae84bb97aee223b1e596ed754819
anandchincholkar19 <anandchincholkar19@gmail.com>
2021-07-23 17:14:53 +0530

    custom input dialog testing

--- app/src/androidTest/java/com/example/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/MainActivityTest.kt
@@ -1,61 +1,48 @@
 package com.example
 
-import android.app.Activity
-import android.app.Instrumentation
-import android.content.Intent
-import android.graphics.BitmapFactory
-import android.os.Bundle
-import android.provider.MediaStore
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.intent.Intents.intending
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
-import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import com.example.exprossotestingdemo.KEY_IMAGE_DATA
 import com.example.exprossotestingdemo.MainActivity
 import com.example.exprossotestingdemo.R
-import org.hamcrest.Matcher
-import org.hamcrest.Matchers.not
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest{
 
-    @get: Rule
-    val intentTestrule = IntentsTestRule(MainActivity::class.java)
-
     @Test
-    fun test_cameraIntent_isBitmapSetToImageView() {
-        // GIVEN
-        val activityResule = createImageActiivtyResultStub()
-        val expectedIntent :Matcher<Intent> = hasAction(MediaStore.ACTION_IMAGE_CAPTURE)
-        intending(expectedIntent).respondWith(activityResule)
+    fun test_showDialog() {
 
-        // Execute and verify
-        onView(withId(R.id.imageView)).check(matches(not(ImageViewDrawableMatcher.hasDrawable())))
+        //GIVEN
+        val activityScenario =  ActivityScenario.launch(MainActivity::class.java)
+        val Expeted_NAME = "Anand"
+
+        //Execute and verify
         onView(withId(R.id.button)).perform(click())
-        intending(expectedIntent)
-        onView(withId(R.id.imageView)).check(matches(ImageViewDrawableMatcher.hasDrawable()))
+
+        onView(withText(R.string.txt_name)).check(matches(isDisplayed()))
+
+        onView(withText(R.string.text_ok)).perform(click())
+
+        onView(withText(R.string.txt_name)).check(matches(isDisplayed()))
+
+        // enter same input
+        onView(withId(R.id.md_input_message)).perform(typeText(Expeted_NAME))
+
+        onView(withText(R.string.text_ok)).perform(click())
+
+        //make sure dialog is gone
+
+        onView(withText(R.string.txt_name)).check(doesNotExist())
+
+        onView(withId(R.id.textView)).check(matches(withText(Expeted_NAME)))
     }
 
-    private fun createImageActiivtyResultStub(): Instrumentation.ActivityResult {
-        val bundle = Bundle()
-        bundle.putParcelable(
-            KEY_IMAGE_DATA,
-            BitmapFactory.decodeResource(
-                intentTestrule.activity.resources,
-                R.drawable.ic_launcher_background
-            )
-        )
-
-        val resultData = Intent()
-        resultData.putExtras(bundle)
-        return Instrumentation.ActivityResult(Activity.RESULT_OK, resultData)
-    }
 
 }
--- app/src/main/java/com/example/exprossotestingdemo/MainActivity.kt
+++ app/src/main/java/com/example/exprossotestingdemo/MainActivity.kt
@@ -1,15 +1,10 @@
 package com.example.exprossotestingdemo
 
-import android.app.Activity
-import android.content.Intent
-import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.input.input
 import kotlinx.android.synthetic.main.activity_main.*
-
-    private val REQUEST_IMAGE_CAPTURE = 1234
-    const val KEY_IMAGE_DATA = "data"
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,31 +13,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button.setOnClickListener {
-            dispatchCameraInent()
+            showDialog()
      }
  }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK) {
-            when(requestCode) {
-                REQUEST_IMAGE_CAPTURE ->{
-                    data?.extras?.let {
-                        extras->
-                        if(extras == null || !extras.containsKey(KEY_IMAGE_DATA)) {
-                            return
-                        }
-                        val imageBitmap = extras[KEY_IMAGE_DATA] as Bitmap?
-                        imageView.setImageBitmap(imageBitmap)
-                    }
+    private fun showDialog(){
+        MaterialDialog(this)
+            .show {
+                input (
+                    waitForPositiveButton = true,
+                    allowEmpty = false
+                ){ dialog, name ->
+                    setNameToTextView(name.toString())
                 }
+                title(R.string.txt_name)
+                positiveButton(R.string.text_ok)
             }
-        }
     }
 
-    private fun dispatchCameraInent() {
-       val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+    private fun setNameToTextView(name: String){
+        textView.text = name
     }
-
 }
